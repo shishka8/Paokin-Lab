bf7147851c5e94b2188768cfef7c842b08b3257a
Grigoriy <gpganin@gmail.com>
2026-03-02 23:25:19 +0300

    88

---
+++ laba8/Form1.Designer.cs
@@ -0,0 +1,208 @@
+﻿namespace labi8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pavilionDataGridView = new System.Windows.Forms.DataGridView();
+            this.pavilionBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.comboBoxOrderBy = new System.Windows.Forms.ComboBox();
+            this.textBoxStatus = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.checkBoxDesc = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionDataGridView)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pavilionDataGridView
+            // 
+            this.pavilionDataGridView.AutoGenerateColumns = false;
+            this.pavilionDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.pavilionDataGridView.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.dataGridViewTextBoxColumn7});
+            this.pavilionDataGridView.DataSource = this.pavilionBindingSource;
+            this.pavilionDataGridView.Location = new System.Drawing.Point(32, 147);
+            this.pavilionDataGridView.Name = "pavilionDataGridView";
+            this.pavilionDataGridView.Size = new System.Drawing.Size(741, 373);
+            this.pavilionDataGridView.TabIndex = 1;
+            // 
+            // pavilionBindingSource
+            // 
+            this.pavilionBindingSource.DataSource = typeof(labi8.FolderModel.Pavilion);
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.DataPropertyName = "Num_pav";
+            this.dataGridViewTextBoxColumn1.HeaderText = "Num_pav";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.DataPropertyName = "ID_mall";
+            this.dataGridViewTextBoxColumn2.HeaderText = "ID_mall";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.DataPropertyName = "Floor";
+            this.dataGridViewTextBoxColumn3.HeaderText = "Floor";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.DataPropertyName = "Status";
+            this.dataGridViewTextBoxColumn4.HeaderText = "Status";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.DataPropertyName = "Square";
+            this.dataGridViewTextBoxColumn5.HeaderText = "Square";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.DataPropertyName = "Cost_meter";
+            this.dataGridViewTextBoxColumn6.HeaderText = "Cost_meter";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // dataGridViewTextBoxColumn7
+            // 
+            this.dataGridViewTextBoxColumn7.DataPropertyName = "Coeff_cost";
+            this.dataGridViewTextBoxColumn7.HeaderText = "Coeff_cost";
+            this.dataGridViewTextBoxColumn7.Name = "dataGridViewTextBoxColumn7";
+            // 
+            // comboBoxOrderBy
+            // 
+            this.comboBoxOrderBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBoxOrderBy.FormattingEnabled = true;
+            this.comboBoxOrderBy.Location = new System.Drawing.Point(32, 90);
+            this.comboBoxOrderBy.Name = "comboBoxOrderBy";
+            this.comboBoxOrderBy.Size = new System.Drawing.Size(121, 21);
+            this.comboBoxOrderBy.TabIndex = 2;
+            // 
+            // textBoxStatus
+            // 
+            this.textBoxStatus.Location = new System.Drawing.Point(639, 90);
+            this.textBoxStatus.Name = "textBoxStatus";
+            this.textBoxStatus.Size = new System.Drawing.Size(100, 20);
+            this.textBoxStatus.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(174, 55);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(71, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Фильтрация";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(518, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(41, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Статус";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(620, 55);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(67, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Сортировка";
+            // 
+            // checkBoxDesc
+            // 
+            this.checkBoxDesc.AutoSize = true;
+            this.checkBoxDesc.Location = new System.Drawing.Point(212, 93);
+            this.checkBoxDesc.Name = "checkBoxDesc";
+            this.checkBoxDesc.Size = new System.Drawing.Size(94, 17);
+            this.checkBoxDesc.TabIndex = 8;
+            this.checkBoxDesc.Text = "По убыванию";
+            this.checkBoxDesc.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(801, 548);
+            this.Controls.Add(this.checkBoxDesc);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxStatus);
+            this.Controls.Add(this.comboBoxOrderBy);
+            this.Controls.Add(this.pavilionDataGridView);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionDataGridView)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pavilionBindingSource)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.BindingSource pavilionBindingSource;
+        private System.Windows.Forms.DataGridView pavilionDataGridView;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn7;
+        private System.Windows.Forms.ComboBox comboBoxOrderBy;
+        private System.Windows.Forms.TextBox textBoxStatus;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.CheckBox checkBoxDesc;
+    }
+}
+
---
+++ laba8/Form1.cs
@@ -0,0 +1,91 @@
+﻿using labi8.FolderModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using labi8.FolderModel;
+namespace labi8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //Экземпляр класса для работы с БД
+        ModelEF database = new ModelEF();
+
+        //список для загрузки в него изначальных (не изменённых) данных
+        List<Pavilion> pavilions = new List<Pavilion>();
+
+        //список для загрузки в него изменённых данных
+        List<Pavilion> PavilionsChange = new List<Pavilion>();
+
+        //Список для загрузки в него полей класса Pavilion
+        List<string> pavilionsProp = new List<string>();
+
+        private void LoadStartData() //загрузка данных списка в источник данных
+        {
+            pavilionBindingSource.DataSource = PavilionsChange;
+        }
+    
+
+        private void LoadDataCombo() //загрузка данных в comboBoxOrderBy
+        {
+            //загружаем поля
+            pavilionsProp = typeof(Pavilion).GetProperties().Select(x => x.Name).ToList();
+            //удаляем поля - связи
+            pavilionsProp.RemoveRange(pavilionsProp.Count - 2, 2);
+            //загружаем полученные данные в comboBoxOrderBy
+            comboBoxOrderBy.DataSource = pavilionsProp;
+            //выбираем первый элемент в comboBoxOrderBy
+            comboBoxOrderBy.SelectedIndex = 0;
+
+        }
+
+        private void LoadOrder() //метод сортировки данных из списка
+        {
+            // (условие) если checkBoxDesc поле Checked имеет значение true -->
+            PavilionsChange = checkBoxDesc.Checked ?
+            //присваивается это значение списка -->
+            PavilionsChange.OrderByDescending(p => p.GetType().GetProperties().First(x => x.Name == comboBoxOrderBy.SelectedItem.ToString()).GetValue(p)).ToList()
+            //иначе это значение списка
+            : PavilionsChange.OrderBy(p => p.GetType().GetProperties().First(x => x.Name == comboBoxOrderBy.SelectedItem.ToString()).GetValue(p)).ToList();
+            LoadStartData(); // -- выполнение метода загрузки данных
+
+        }
+
+        //событие загрузки формы
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            //загружаем данные в списки из БД
+            PavilionsChange = pavilions = database.Pavilion.ToList();
+            //вызываем методы загрузки
+            LoadStartData();
+            LoadDataCombo();
+        }
+
+        private void Data_changes(object sender, EventArgs e)
+        {
+            LoadOrder(); //вызываем метод сортировки данных
+
+        }
+
+        //Это событие возникает, если Text свойство элемента textBoxStatus изменяется
+        private void textBoxStatus_TextChanged(object sender, EventArgs e)
+        {
+            //поиск pavilions по списку где по полю статус содержится текст из textBoxStatus
+            PavilionsChange = pavilions.Where(x => x.Status.Contains(textBoxStatus.Text)).ToList();
+            //вызываем метод сортировки данных
+            LoadOrder();
+
+        }
+    }
+}
